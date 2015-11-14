08ebdcc29a84576a12524a868752c5be4208fc9e
liansanyue <1448784313@qq.com>
2015-11-14 14:19:06 +0800

    将main.js用jquery重写

--- js/main.js
+++ js/main.js
@@ -1,84 +1,69 @@
-var imgplay = document.getElementById("left_imgplay");
-var playimg = imgplay.getElementsByTagName("img")[0];
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
-var list = document.getElementsByClassName("nav_list");
-var alist = document.getElementsByClassName("range");
-var imgas = document.getElementsByClassName("img");
-var img_is = document.getElementsByClassName("playicon");
-var sname = document.getElementById("left_recommend").getElementsByClassName("ellipsis mname");
+$(".img").each(function(index) {
 
-  
-(function() {
+    $(this).mouseover(function() {
+        $(this).find("i").css({
+            "display": "inline-block",
+            "color": "#fff"
+        });
 
-    for (var i = 0; i < imgas.length; i++) {
-        (function(i) {
-            imgas[i].onmouseover = function() {
+    });
 
-                img_is[i].style.display = "inline-block";
-                img_is[i].style.color = "#fff";
+    $(this).mouseout(function() {
+        $(this).find("i").css("display", "none");
 
-            }
-        })(i);
-        (function(i) {
-            imgas[i].onmouseout = function() {
+    });
+});
 
-                img_is[i].style.display = "none";
-            }
-        })(i);
+$("#left_recommend .mname").each(function(index) {
+    var snamelength = $(this).text().length;
+    if (snamelength > 12) {
+        var newsname = $(this).text().substr(0, snamelength) + '...';
+        $(this).text(newsname);
     }
+});
 
-
-})()
-
-for(var i=0;i<sname.length;i++){
-  
- var snamelength = sname[i].innerHTML.length; 
-    if(snamelength>12){
-        var newsname= sname[i].innerHTML.substr(0,snamelength)+'...';
-        sname[i].innerHTML = newsname;
-    }
-}
-
+/*图片轮播效果----------------------------------------------------------------------*/
 var liveflag = 1,
     preflag = 0;
+var playimg = $("#playimg");
 
 function change(num) {
 
-    playimg.src = "img/" + (num) + ".jpg";
+    playimg.attr("src", "img/" + (num) + ".jpg");
     liveflag = num + 1;
-    for (var i = 0; i < alist.length; i++) {
-        if (i == num) {
-            alist[i].style.backgroundColor = "#eee";
+    $(".range").each(function(index) {
+        if (index == num) {
+            $(this).css({
+                "background-color": "#eee"
+            });
         } else {
-            alist[i].style.backgroundColor = "rgba(161, 161, 161, 0.5)";
+            $(this).css({
+                "background-color": "rgba(161, 161, 161, 0.5)"
+            });
         }
-    }
-
+    });
 }
 
-prev.onclick = function() {
-
+/*图片前后切换*/
+$("#prev").click(function() {
     previmg();
+})
 
-
-}
-
-next.onclick = function() {
-
+$("#next").click(function() {
     nextimg();
+})
 
-}
 
 function nextimg() {
     if (liveflag == 8) {
         liveflag = 0;
     }
 
+    playimg.attr("src", "img/" + (liveflag) + ".jpg");
 
-    playimg.src = "img/" + (liveflag) + ".jpg";
-    alist[preflag].style.backgroundColor = "rgba(161, 161, 161, 0.5)";
-    alist[liveflag].style.backgroundColor = "#eee";
+
+    $(".range:eq(" + preflag + ")").css("background-color", "rgba(161, 161, 161, 0.5)");
+    $(".range:eq(" + liveflag + ")").css("background-color", "#eee");
     preflag = liveflag;
     liveflag++;
 
@@ -88,35 +73,44 @@ function previmg() {
     if (liveflag == -1) {
         liveflag = 7;
     }
+    playimg.attr("src", "img/" + (liveflag) + ".jpg");
 
-    playimg.src = "img/" + (liveflag) + ".jpg";
-    alist[preflag].style.backgroundColor = "rgba(161, 161, 161, 0.5)";
-    alist[liveflag].style.backgroundColor = "#eee";
+    $(".range:eq(" + preflag + ")").css("background-color", "rgba(161, 161, 161, 0.5)");
+    $(".range:eq(" + liveflag + ")").css("background-color", "#eee");
     preflag = liveflag;
     liveflag--;
 
 }
-
-function show(num) {
-    list[num].style.display = "";
-    list[num].style.position = "absolute";
-    list[num].style.left = 238 + num * 110 + "px";
-    for (var i = 0; i < list.length; i++) {
-        if (i != num) {
-            list[i].style.display = "none";
-            list[i].style.paddingLeft = "";
-        }
-    }
-}
+/*定时*/
+var imgplay = $("#left_imgplay");
 var play = setInterval(function() {
     nextimg();
 }, 2000);
-imgplay.onmouseover = function() {
+$("#left_imgplay").mouseover(function() {
     clearInterval(play);
-}
+});
 
-imgplay.onmouseout = function() {
+$("#left_imgplay").mouseout(function() {
     play = setInterval(function() {
         nextimg();
     }, 2000);
+});
+/*图片轮播效果----------------------------------------------------------------------*/
+/*导航栏*/
+function show(num) {
+
+    $(".nav_list:eq(" + num + ")").css({
+        "display": "",
+        "position": "absolute",
+        "left": 238 + num * 110 + "px"
+    });
+    $(".nav_list").each(function(index) {
+
+        if (index != num) {
+
+            $(".nav_list:eq(" + index + ")").css({
+                "display": "none"
+            });
+        }
+    })
 }
